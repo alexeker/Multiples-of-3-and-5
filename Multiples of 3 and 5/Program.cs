1470ab813a6d45970d54f4d55733dcb4defa78ff
Alex Kuripko <alexeu.kyripko@gmail.com>
2020-01-07 18:50:49 +0200

    Добавьте файлы проекта.

---
+++ Multiples of 3 and 5/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Программа ищет в числах до n числа, который кратны 3 или 5
+ * Затем находит сумму этих чисел
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace Multiples_of_3_and_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sum = 0;//Объявляем сумму
+            var n = 1000;
+            for (int i =0;i<n;++i)
+            {
+                //Проходим по всем числам до n...
+                if(i%3==0 || i % 5 == 0)
+                {
+                    //Проверяем условие кратности
+                    sum += i;//Суммируем 
+                
+                }
+            }
+            Console.WriteLine(sum);//Вывод
+            Console.ReadKey();
+        }
+    }
+}
